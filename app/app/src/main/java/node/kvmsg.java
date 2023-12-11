b566499b0672a60e949ca66f1e268b49461ead85
Malva2312 <up202006605@edu.fe.up.pt>
2023-12-11 20:51:25 +0000

    Fix props.clear() bug in kvmsg class

--- app/app/src/main/java/node/kvmsg.java
+++ app/app/src/main/java/node/kvmsg.java
@@ -57,8 +57,10 @@ public class kvmsg
         byte[] msg = frame[FRAME_PROPS];
         props_size = msg.length;
         props.clear();
+        if (msg.length == 0)
+            return;
 
-        System.out.println("" + msg.length + " :" + new String(msg, ZMQ.CHARSET));
+        //System.out.println("" + msg.length + " :" + new String(msg, ZMQ.CHARSET));
         for (String prop : new String(msg, ZMQ.CHARSET).split("\n")) {
             String[] split = prop.split("=");
             props.setProperty(split[0], split[1]);
